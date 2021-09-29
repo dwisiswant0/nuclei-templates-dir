3bbfcb4302e9c9ca735f5d953fa3917993b9dca3
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-09-29 21:29:54 +0700

    Add dialog modal

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-function init() {
-	let req = new XMLHttpRequest();
-	req.open("GET", "db.json", true);
-	req.send();
-	req.addEventListener("readystatechange", function() {
-		if (this.readyState === this.DONE && this.status === 200) {
-			try {
-				window.db = JSON.parse(this.response)
-			} catch(e) {
-				alert("Error! Database can't be loaded: " + e.message)
-			}
-		}
-	});
+var contributors = document.getElementById("contributors"),
+	count = document.getElementById("count"),
+	dialog = document.getElementById("dialog"),
+	dialogMsg = document.getElementById("dialog-message"),
+	keyword = document.getElementById("keyword"),
+	list = document.getElementById("list"),
+	result = document.getElementById("result"),
+	search = document.getElementById("search"),
+	top10 = document.getElementById("top-10");
 
-	loadTop10()
-}
-
-String.prototype.toHtmlEntities = function() {
+String.prototype.toHtmlEntities = function() {	
 	return this.replace(/./gm, function(s) {
 		return (s.match(/[a-z0-9\s-_]+/i)) ? s : "&#" + s.charCodeAt(0) + ";";
 	});
@@ -27,12 +20,24 @@ String.prototype.escapeRegExp = function() {
 	});
 }
 
-var search = document.getElementById("search"),
-	result = document.getElementById("result"),
-	keyword = document.getElementById("keyword"),
-	count = document.getElementById("count"),
-	list = document.getElementById("list"),
-	top10 = document.getElementById("top-10");
+function init() {
+	let req = new XMLHttpRequest();
+	req.open("GET", "db.json", true);
+	req.send();
+	req.addEventListener("readystatechange", function() {
+		if (this.readyState === this.DONE) {
+			try {
+				window.db = JSON.parse(this.response);
+				search.removeAttribute("disabled")
+			} catch(e) {
+				dialogMsg.innerText = "Database can't be loaded: " + e.message;
+				dialog.showModal()
+			}
+		}
+	});
+
+	loadTop10()
+}
 
 function loadTop10() {
 	let req = new XMLHttpRequest();
@@ -42,13 +47,14 @@ function loadTop10() {
 		if (this.readyState === this.DONE && this.status === 200) {
 			var list = Array.from(this.response.matchAll(/^\|\s\w+.+?\|\s+\d+\s\|\s(\w+)/gm)).map(match => match[1]),
 				output = "";
-			console.log(list);
+
 			list.forEach(function(user) {
 				output += `<a class="contributor" href="https://github.com/${user}" target="_blank">` +
 					`<img class="nes-avatar is-large is-rounded lazy" src="//github.com/${user}.png?size=64" onerror="this.onerror=null, this.src='//github.com/github.png?size=64'">` +
 					`<p>${user}</p></a>\n`
 			});
-			top10.innerHTML = output
+			top10.innerHTML = output;
+			contributors.style.display = "block"
 		}
 	})
 }
